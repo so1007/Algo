470057fd92b68e7cc7f819d1b6681f2946164d0d
MengxiZhang <zhmx1828@sina.com>
2016-07-08 02:24:56 +0800

    Add MergeSort algo to file sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// BubbleSort input params: len=length of array
 void BubbleSort(int arr[],int len){
 	for(int i=0;i<len-1;++i){
 		for(int j=1;j<len-i;++j){
@@ -13,6 +14,7 @@ void BubbleSort(int arr[],int len){
 	}
 }
 
+//InsertSort input params: len=length of array
 void InsertSort(int arr[], int len){
 	for(int i=1;i<len;++i){
 		int temp=arr[i];
@@ -31,6 +33,7 @@ void InsertSort(int arr[], int len){
 	}
 }
 
+// QuickSort input params: low=0, high=length of array-1
 int Partition(int arr[], int low, int high){
 	int temp=arr[low];
 	while(low<high){
@@ -49,11 +52,41 @@ void QSort(int arr[],int low, int high){
 	QSort(arr,pivot+1,high);
 }
 
+// MergeSort input len: length of array
+void merge(int arr[],int aux[], int low, int mid, int high){
+	for(int i=low;i<=high;++i){
+		aux[i]=arr[i];
+	}
+	int i=low,j=mid+1;
+	for(int k=low;k<=high;++k){
+		if(i>mid) arr[k]=aux[j++];
+		else if(j>high) arr[k]=aux[i++];
+		else if(aux[i]>=aux[j]) arr[k]=aux[j++];
+		else arr[k]=aux[i++];
+	}
+}
+void MSort(int arr[], int aux[], int low, int high){
+	if(low>=high) return;
+	int mid=(low+high)/2;
+	MSort(arr,aux,low,mid);
+	MSort(arr,aux,mid+1,high);
+	merge(arr,aux,low,mid,high);
+}
+void MergeSort(int arr[], int len){
+	int* aux=new int[len];
+
+	MSort(arr,aux,0,len-1);
+
+	delete[] aux;
+}
+// print array
 void print(int arr[], int len){
 	for(int i=0;i<len;++i)
 		cout<<arr[i]<<" ";
 	cout<<endl;
 }
+
+//test sort algorithm
 int main(){
 	int array[50];
 	int length;
@@ -62,10 +95,11 @@ int main(){
 			cin>>array[i];
 		//BubbleSort(array,length);
 		//InsertSort(array,length);
-		QSort(array,0,length-1);
+		//QSort(array,0,length-1);
+		MergeSort(array,length);
 		print(array,length);
 	}
-	
+
 	system("pause");
 	return 0;
 }
